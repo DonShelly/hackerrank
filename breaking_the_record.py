13e8818e58968e533785b6a37447fbec6e44c04c
Adrian <adrian.elson@version1.com>
2023-04-07 15:44:29 +0100

    comp tc, btr in progress

---
+++ breaking_the_record.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'breakingRecords' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY scores as parameter.
+#
+
+def breakingRecords(scores):
+    # Write your code here
+    record_high = scores[0]
+    record_low = scores[0]
+    count_record_high = 0
+    count_record_low = 0
+    for score in scores:
+
+
+    return ret
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    result = breakingRecords(scores)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+
+    fptr.close()
